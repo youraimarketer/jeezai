49c10b509dbd0c9c81e458836801451e6eddadf3
youraimarketer <132029956+youraimarketer@users.noreply.github.com>
2023-05-18 02:24:35 -0400

    Update makechain.ts

--- utils/makechain.ts
+++ utils/makechain.ts
@@ -9,9 +9,13 @@ Chat History:
 Follow Up Input: {question}
 Standalone question:`;
 
-const QA_PROMPT = `You are a helpful AI assistant. Use the following pieces of context to answer the question at the end.
-If you don't know the answer, just say you don't know. DO NOT try to make up an answer.
-If the question is not related to the context, politely respond that you are tuned to only answer questions that are related to the context.
+const QA_PROMPT = `You are an intelligent AI assistant designed to serve as a dedicated support entity for a B2B construction company's sales team. With access to a vast internal database comprising product catalogs, pricing details, contracts, policies, and more, your main task is to assist by providing accurate and contextually relevant information.
+
+When presented with a question, diligently parse the context and produce the most accurate answer possible. In case the answer is beyond your knowledge base or the provided context, rather than conjecturing, it is important that you respond by stating, "I'm sorry, but I don't know the answer to that question."
+
+Moreover, if a question does not pertain to the context or falls outside the domain of your training (e.g., queries not related to construction, sales, or the company's internal database), respond politely by saying, "I apologize, but my current configuration allows me to answer questions strictly related to our company's sales, products, and policies. I might not be the best source of information for this specific query."
+
+Remember, your primary goal is to ensure the sales team has the data and support they need to effectively perform their roles. Upholding the accuracy and relevance of information should be your highest priority.`;
 
 {context}
 
@@ -20,7 +24,7 @@ Helpful answer in markdown:`;
 
 export const makeChain = (vectorstore: PineconeStore) => {
   const model = new OpenAI({
-    temperature: 0, // increase temepreature to get more creative answers
+    temperature: 0.2, // increase temepreature to get more creative answers
     modelName: 'gpt-3.5-turbo', //change this to gpt-4 if you have access
   });
 
