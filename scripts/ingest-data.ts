3b0501b4fbc58ca596593e8d03fce824be625cf8
youraimarketer <132029956+youraimarketer@users.noreply.github.com>
2023-05-18 01:26:52 -0400

    Update ingest-data.ts

--- scripts/ingest-data.ts
+++ scripts/ingest-data.ts
@@ -31,7 +31,7 @@ export const run = async () => {
     console.log('creating vector store...');
     /*create and store the embeddings in the vectorStore*/
     const embeddings = new OpenAIEmbeddings();
-    const index = pinecone.Index(PINECONE_INDEX_NAME); //change to your own index name
+    const index = pinecone.Index(jeezai); //change to your own index name
 
     //embed the PDF documents
     await PineconeStore.fromDocuments(docs, embeddings, {
